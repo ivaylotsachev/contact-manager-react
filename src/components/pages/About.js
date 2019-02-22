fce69ec3805f00966124643e2fca4adc04aa655d
ivaylo <itsa4ev@gmail.com>
2019-02-22 22:00:55 +0200

    Create about us page

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="display-4">About Contact Manager</h2>
+      <p className="lead">Simlpe app to manage contacts</p>
+      <p className="lead">Using React Context, React Router, Bootstrap</p>
+      <p className="text-secondary">Verion 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
