9c301555843b7d2933faae73089dd974f0d2f754
ivaylo <itsa4ev@gmail.com>
2019-02-23 21:30:43 +0200

    Updates

---
+++ precache-manifest.cca87c0a92c2780d67a638e927e7c40c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2e7adc7343cd87f9baa",
+    "url": "/contact-manager-react/static/js/runtime~main.e2e7adc7.js"
+  },
+  {
+    "revision": "e395a11b61cee483689a",
+    "url": "/contact-manager-react/static/js/main.e395a11b.chunk.js"
+  },
+  {
+    "revision": "1edefa28f4e4744cf426",
+    "url": "/contact-manager-react/static/js/2.1edefa28.chunk.js"
+  },
+  {
+    "revision": "e395a11b61cee483689a",
+    "url": "/contact-manager-react/static/css/main.210b0475.chunk.css"
+  },
+  {
+    "revision": "1edefa28f4e4744cf426",
+    "url": "/contact-manager-react/static/css/2.985ecec6.chunk.css"
+  },
+  {
+    "revision": "55a06a69f031e3cd89e17786328ab70c",
+    "url": "/contact-manager-react/index.html"
+  }
+];
