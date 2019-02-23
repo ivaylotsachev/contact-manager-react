9bfd116e4d5cfb2fee4cbfbfe721ae2b3dfd2322
ivaylo <itsa4ev@gmail.com>
2019-02-23 21:34:38 +0200

    Change title

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,9 @@ const About = () => {
       <h2 className="display-4">About Contact Manager</h2>
       <hr />
       <p className="lead">Simlpe app to manage contacts</p>
-      <p className="lead">Using React Context, React Router, Bootstrap</p>
+      <p className="lead">
+        Using React Context to manage state, React Router and Bootstrap 4
+      </p>
       <p className="text-secondary">Verion 1.0.0</p>
     </div>
   );
